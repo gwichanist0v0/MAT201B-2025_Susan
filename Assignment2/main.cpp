cccf76c01b5217e7dca139031bd2b5c25e5f6f5f
Susan <xzhong@berklee.edu>
2025-04-16 15:12:12 -0700

    susan assignment 2

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -3,6 +3,7 @@
 #include "al/graphics/al_Shapes.hpp" // addCone, addCube, addSphere
 #include "al/types/al_Color.hpp"
 #include "al/math/al_Interpolation.hpp"
+#include "al/math/al_Random.hpp"
 
 using namespace al;
 
@@ -17,7 +18,7 @@ std::string slurp(std::string fileName); // only a declaration
 
 class MyApp : public App{
 
-    Mesh grid, rgb, hsv, mine;
+    Mesh grid, rgb, hsv, test;
     Mesh mesh; 
     ShaderProgram shader;
     Parameter pointSize{"pointSize", 0.005, 0.005, 0.005 };
@@ -25,9 +26,24 @@ class MyApp : public App{
     Mesh::Vertices meshVertices;
     Mesh::Vertices gridVertices; 
     Mesh::Vertices rgbVertices;
+    Mesh::Vertices hsvVertices;
+    Mesh::Vertices testVertices;
 
+    bool animStart = false; 
 
     Image image; 
+
+    enum AnimationType {
+    NONE,
+    ANIM1,
+    ANIM2,
+    ANIM3,
+    ANIM4 
+    };
+
+    AnimationType currentAnimation = NONE;
+    double timeSinceAnimStart = 0;
+    double animDuration = 0;
 
     void onInit() override{
 
@@ -41,8 +57,6 @@ class MyApp : public App{
 
     void onCreate() override {
 
-        //makeImage(); 
-
         nav().pos(0, 0, 5);
 
         if (! shader.compile(slurp("../point-vertex.glsl"), slurp("../point-fragment.glsl"), slurp("../point-geometry.glsl"))) {
@@ -54,17 +68,19 @@ class MyApp : public App{
         mesh.primitive(Mesh::POINTS);
         grid.primitive(Mesh::POINTS);
         rgb.primitive(Mesh::POINTS);
-
+        test.primitive(Mesh::POINTS);
+        hsv.primitive(Mesh::POINTS);
+        
         for (int y = 0; y < image.height(); ++y) {
             for (int x = 0; x < image.width(); ++x) {
                 auto pixel = image.at(x, y);
                 mesh.vertex(float(x) / image.width(), float(-y) / image.width(), 0);
                 mesh.color(pixel.r / 255.0, pixel.g / 255.0, pixel.b / 255.0);
-                mesh.texCoord(0.1, 0);
+                mesh.texCoord(0.5, 0);
 
                 grid.vertex(float(x) / image.width(), float(-y) / image.width(), 0);
                 grid.color(pixel.r / 255.0, pixel.g / 255.0, pixel.b / 255.0);
-                grid.texCoord(0.1, 0);
+                grid.texCoord(0.5, 0);
 
                 float r = pixel.r / 255.0;
                 float g = pixel.g / 255.0;
@@ -72,9 +88,24 @@ class MyApp : public App{
 
                 rgb.vertex(r, g, b);
                 rgb.color(r, g, b);
-                rgb.texCoord(0.1, 0);
+                rgb.texCoord(0.5, 0);
 
-                HSV foo(RGB(1, 0, 1));
+                test.vertex(float(x) / image.width(), float(y) / image.width(), 0);
+                test.color(pixel.r / 255.0, pixel.g / 255.0, pixel.b / 255.0);
+                test.texCoord(0.5, 0);
+
+                HSV hsvPoints(RGB(r, g, b));
+                // Convert HSV to XYZ coordinates on a cylinder
+                float angle = hsvPoints.h * 2.0f * M_PI; // Convert hue to radians
+                float radius = hsvPoints.s; // Use saturation as radius
+                float height = hsvPoints.v; // Use value as height
+                
+                // Convert polar coordinates to cartesian
+                float x_coord = radius * cos(angle);
+                float y_coord = radius * sin(angle);
+                float z_coord = height;
+                
+                hsv.vertex(x_coord, y_coord, z_coord);
 
 
             }
@@ -84,48 +115,44 @@ class MyApp : public App{
 
         meshVertices = mesh.vertices();
         gridVertices = grid.vertices();
-        rgbVertices = rgb.vertices();
-
+        rgbVertices = rgb.vertices();   
+        testVertices = test.vertices();
+        hsvVertices = hsv.vertices();
     }
 
     double time = 0;
 
+
     void onAnimate(double dt) override {
-        time += dt;
+    if (currentAnimation != NONE) {
+        timeSinceAnimStart += dt;
 
-    
-        if (time < 2 && time > 1) {
-         
-            std::cout << time << std::endl;  
+        float t = timeSinceAnimStart / animDuration;
+        if (t > 1.0f) {
+            currentAnimation = NONE;
+            return;
+        }
 
-
-            // std::cout << meshVertices.size() << std::endl;  
-            // std::cout << rgbVertices.size() << std::endl;  
-
-            for (int i = 0; i < meshVertices.size(); ++i) {
-                
-                auto endVertices = ipl::linear(time, meshVertices[i], rgbVertices[i]); 
-                mesh.vertex(endVertices); 
-
-
-
-                // std::cout << meshVertices[i] << std::endl;  
-                // std::cout << rgbVertices[i] << std::endl;  
-                
-                //meshVertice[i] ... rgb grid etc 
-
-
-                
+        for (int i = 0; i < mesh.vertices().size(); ++i) {
+            Vec3f a = meshVertices[i];  // start
+            Vec3f b;
+            
+            if (currentAnimation == ANIM1) {
+                b = gridVertices[i];
+            } else if (currentAnimation == ANIM2) {
+                b = hsvVertices[i];
+            } else if (currentAnimation == ANIM3) {
+                b = rgbVertices[i];
+            } else if (currentAnimation == ANIM4) {
+                b = testVertices[i];
             }
 
+            Vec3f c = a * (1.0f - t) + b * t;
+            mesh.vertices()[i] = c;
         }
-        //update `mesh` to be a mix of two other meshes
-        // for each vertex of `mesh`
-        //   vertex = mix(A, B)
-        //
-
-
     }
+}
+
 
     void onDraw(Graphics& g) override {
         g.clear(0.1);
@@ -143,24 +170,34 @@ class MyApp : public App{
             quit();
         }
 
+        meshVertices = mesh.vertices();        
 
         if (k.key() == '1') {
-            time = 0;
+            currentAnimation = ANIM1;
+            animDuration = 1.0;
+            timeSinceAnimStart = 0;
         }
 
-        if (k.key() == '2'){
-            
-            time = 1; 
-
+        if (k.key() == '2') {
+            currentAnimation = ANIM2;
+            animDuration = 2.0;
+            timeSinceAnimStart = 0;
         }
 
-    
+        if (k.key() == '3') {
+            currentAnimation = ANIM3;
+            animDuration = 3.0;
+            timeSinceAnimStart = 0;
+        }
+
+        if (k.key() == '4') {
+            currentAnimation = ANIM4;
+            animDuration = 4.0;
+            timeSinceAnimStart = 0;
+        }
 
         return true;
     }
-
-
-    
 
 }; 
 
